381019d13e2baadc11f175bf178bd2a3b567c5e1
MIGUEL ANGEL LOPEZ RENDON <PABLNINN18@CORPORATIVO.GRUPOREYES.ORG>
2025-12-24 11:52:28 -0600

    Added icons

---
+++ src/constants/icons.ts
@@ -0,0 +1,11 @@
+import homeIcon from '@/assets/icons/home.png'
+import inventoryIcon from '@/assets/icons/inventory.png'
+import storeIcon from '@/assets/icons/store.png'
+
+export const ICONS = {
+  HOME: homeIcon,
+  INVENTORY: inventoryIcon,
+  STORE: storeIcon
+}
+
+export default ICONS
