a7e91cdfe979cf239b45696fec81816eaa14f531
MIGUEL ANGEL LOPEZ RENDON <PABLNINN18@CORPORATIVO.GRUPOREYES.ORG>
2025-12-27 13:38:06 -0600

    global accepting for ts

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div class="app-container">
     <SideBarComponent 
     :toggled="toggled"
-    @changeViewSignal="(module) => console.log('View changed to:', module.nombre)"
+    @changeViewSignal="(module: Module) => console.log('View changed to:', module.nombre)"
      />
     <ComponentContainerComponent @toggle-sidebar="handleToggleSidebar" />
   </div>
@@ -12,6 +12,7 @@
 import { ref } from 'vue';
 import ComponentContainerComponent from './components/ComponentContainerComponent.vue';
 import SideBarComponent from './components/SideBarComponent.vue';
+import type { Module } from './models/module';
 
 const toggled = ref(true);
 
